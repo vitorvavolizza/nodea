d4001337d2324dedb3883a7c81b5173ab713b979
vitor <vitorvavolizza@gmail.com>
2024-04-30 16:22:16 -0700

    changed prompt

--- app/dashboard/TextUpdater.tsx
+++ app/dashboard/TextUpdater.tsx
@@ -38,70 +38,72 @@ function TextUpdaterNode({ data, id, isConnectable, onSubmit }: any) {
     deleteElements({ nodes: [{ id }] });
   }, [id, deleteElements]);
 
-
   return (
-      <HoverCard>
-        <div className=" bg-white shadow-lg rounded-lg p-4 w-[300px]">
+    <HoverCard>
+      <div className=" bg-white shadow-lg rounded-lg p-4 w-[320px]">
+        <Handle
+          type="target"
+          position={Position.Left}
+          isConnectable={isConnectable}
+        />
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            {id === "node-1" ? (
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Enter the initial idea
+              </label>
+            ) : null}
+            <textarea
+              id="text"
+              name="text"
+              value={value}
+              onChange={onChange}
+              onKeyDown={handleKeyDown}
+              className="nodrag mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-xs"
+              placeholder="Type something..."
+              rows={6}
+            />
+
+            <Button
+              type="button"
+              className="mt-2 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm leading-4 font-medium rounded-xl shadow-sm text-indigo-600 bg-white hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={deleteNode}
+            >
+              Delete
+            </Button>
+
+            <Button
+              type="submit"
+              className="mt-2 ml-2 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm leading-4 font-medium rounded-xl shadow-sm text-indigo-600 bg-white hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Generate
+            </Button>
+          </div>
+
           <Handle
-            type="target"
-            position={Position.Left}
+            type="source"
+            position={Position.Right}
+            id="a"
             isConnectable={isConnectable}
           />
-          
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              {id === "node-1" ? (
-                <label
-                  htmlFor="text"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Enter the initial idea
-                </label>
-              ) : null}
-              <textarea
-                id="text"
-                name="text"
-                value={value}
-                onChange={onChange}
-                onKeyDown={handleKeyDown}
-                className="nodrag mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 text-xs"
-                placeholder="Type something..."
-                rows={6}
-              />
-
-              <Button type="button" className="mt-2 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm leading-4 font-medium rounded-xl shadow-sm text-indigo-600 bg-white hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" onClick={deleteNode} >
-                Delete
-              </Button>
-
-              <Button type="submit" className="mt-2 ml-2 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm leading-4 font-medium rounded-xl shadow-sm text-indigo-600 bg-white hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Generate
-              </Button>
-
-              
-            </div>
-            
-
-            <Handle
-              type="source"
-              position={Position.Right}
-              id="a"
-              isConnectable={isConnectable}
-            />
-          </form>
-          <HoverCardContent>
-            <Button variant="ghost" className="text-gray-600 font-light">
-              Add connected node
-            </Button>
-            <Button variant="ghost" className="text-gray-600 font-light">
-              Generate ideas
-            </Button>
-            <Button variant="ghost" className="text-gray-600 font-light">
-              Delete node
-            </Button>
-          </HoverCardContent>
-        </div>
-      </HoverCard>
-    
+        </form>
+        <HoverCardContent>
+          <Button variant="ghost" className="text-gray-600 font-light">
+            Add connected node
+          </Button>
+          <Button variant="ghost" className="text-gray-600 font-light">
+            Generate ideas
+          </Button>
+          <Button variant="ghost" className="text-gray-600 font-light">
+            Delete node
+          </Button>
+        </HoverCardContent>
+      </div>
+    </HoverCard>
   );
 }
 
--- components/export.tsx
+++ components/export.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Panel, useReactFlow, getRectOfNodes, getTransformForBounds } from 'reactflow';
-import { toPng } from 'html-to-image';
+import React from "react";
+import {
+  Panel,
+  useReactFlow,
+  getRectOfNodes,
+  getTransformForBounds,
+} from "reactflow";
+import { toPng } from "html-to-image";
 import { Button } from "@/components/ui/button";
 
 function downloadImage(dataUrl) {
-  const a = document.createElement('a');
+  const a = document.createElement("a");
 
-  a.setAttribute('download', 'reactflow.png');
-  a.setAttribute('href', dataUrl);
+  a.setAttribute("download", "nodea_export.png");
+  a.setAttribute("href", dataUrl);
   a.click();
 }
 
@@ -21,10 +26,16 @@ function DownloadButton() {
     // we then overwrite the transform of the `.react-flow__viewport` element
     // with the style option of the html-to-image library
     const nodesBounds = getRectOfNodes(getNodes());
-    const transform = getTransformForBounds(nodesBounds, imageWidth, imageHeight, 0.5, 2);
+    const transform = getTransformForBounds(
+      nodesBounds,
+      imageWidth,
+      imageHeight,
+      0.5,
+      2
+    );
 
-    toPng(document.querySelector('.react-flow__viewport'), {
-      backgroundColor: '#1a365d',
+    toPng(document.querySelector(".react-flow__viewport"), {
+      backgroundColor: "#1a365d",
       width: imageWidth,
       height: imageHeight,
       style: {
@@ -36,9 +47,12 @@ function DownloadButton() {
   };
 
   return (
-      <Button  className="mt-2 ml-2 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm leading-4 font-medium rounded-xl shadow-sm text-indigo-600 bg-white hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 fixed top-0 right-2" onClick={onClick}>
-        Download Image
-      </Button>
+    <Button
+      className="mt-2 ml-2 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm leading-4 font-medium rounded-xl shadow-sm text-indigo-600 bg-white hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 fixed top-0 right-2"
+      onClick={onClick}
+    >
+      Download Image
+    </Button>
   );
 }
 
